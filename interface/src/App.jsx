b59f0433beb11437d1b1694cec893e6ebd1adaa6
xz <luc_xz@foxmail.com>
2025-02-19 16:49:37 +0800

    feat: init

---
+++ interface/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { XProvider } from '@ant-design/x';
+
+function App() {
+  return (
+    <XProvider>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </XProvider>
+  )
+}
+
+export default App
