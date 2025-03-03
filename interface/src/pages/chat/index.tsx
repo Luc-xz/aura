543537eb0c8cc62c28bcc88bcdbd06adeb88b3c7
xz <luc_xz@foxmail.com>
2025-03-03 15:43:35 +0800

    feat: chat原型

--- interface/src/pages/chat/index.tsx
+++ interface/src/pages/chat/index.tsx
@@ -1,8 +1,21 @@
-import { Button, Card, Divider } from 'antd'
-import { PlusOutlined, CaretLeftFilled, CaretRightFilled, SwapOutlined, SettingOutlined } from '@ant-design/icons'
-import { useState } from 'react'
+import { Button, Card, Divider, Flex, Space } from 'antd'
+import { Bubble, Attachments, Sender } from '@ant-design/x'
+import {
+  PlusOutlined,
+  CaretLeftFilled,
+  CaretRightFilled,
+  SwapOutlined,
+  SettingOutlined,
+  UserOutlined,
+  MehOutlined,
+  SyncOutlined,
+  CopyOutlined,
+  CloudUploadOutlined,
+  LinkOutlined,
+} from '@ant-design/icons'
+import { useState, useEffect } from 'react'
 
-function ChatHistory() {
+function HistoryPanel({ activeChat, setActiveChat }) {
   const [history, setHistory] = useState([
     {
       id: 1,
@@ -12,12 +25,12 @@ function ChatHistory() {
       updatedAt: '2021-01-01 12:00:00',
       data: [
         {
-          proposer: 'bot',
+          proposer: 'user',
           content: '你好，我是小明，很高兴认识你',
           time: '2021-01-01 12:00:00',
         },
         {
-          proposer: 'user',
+          proposer: 'bot',
           content: '你好，小明，很高兴认识你',
           time: '2021-01-01 12:00:00',
         },
@@ -31,35 +44,38 @@ function ChatHistory() {
       updatedAt: '2021-01-01 12:00:00',
       data: [
         {
-          proposer: 'bot',
+          proposer: 'user',
           content: '你好，我是小明，很高兴认识你',
           time: '2021-01-01 12:00:00',
         },
         {
-          proposer: 'user',
+          proposer: 'bot',
           content: '你好，小明，很高兴认识你',
           time: '2021-01-01 12:00:00',
         },
       ],
     },
   ])
-  const [activeChat, setActiveChat] = useState(history[0])
   const [historyVisible, setHistoryVisible] = useState(true)
 
   const HistoryCardList = history.map((item) => (
     <div
       key={item.id}
       onClick={() => setActiveChat(item)}
-      className={`p-3 w-full space-x-2  cursor-pointer ${item.id === activeChat.id ? 'card-active' : 'card-inactive'}`}>
+      className={`p-3 w-full space-x-2  cursor-pointer ${item.id === (activeChat && activeChat.id) ? 'card-active' : 'card-inactive'}`}>
       <div className="flex items-center">
         <div className="flex flex-col justify-between">
           <div>{item.title}</div>
           <div className="text-sm text-gray-500">{item.model}</div>
         </div>
-        {item.id === activeChat.id && <div className="relative bottom-1 flex-1 text-xl text-right cursor-pointer">...</div>}
+        {item.id === (activeChat && activeChat.id) && <div className="relative bottom-1 flex-1 text-xl text-right cursor-pointer">...</div>}
       </div>
     </div>
   ))
+
+  useEffect(() => {
+    setActiveChat(history[0])
+  }, [])
 
   return (
     <div className="relative bg-moon border-r border-border flex-0">
@@ -85,13 +101,42 @@ function ChatHistory() {
   )
 }
 
-function Chat() {
+function ChatPanel({ activeChat }) {
   const [model, setModel] = useState({
     name: 'DeepSeek-R1',
   })
 
+  const chatBubbleList = activeChat
+    ? activeChat.data.map((item) => (
+        <Bubble
+          placement={item.proposer === 'user' ? 'end' : 'start'}
+          content={item.content}
+          avatar={{ icon: item.proposer === 'user' ? <UserOutlined /> : <MehOutlined /> }}
+          footer={
+            item.proposer === 'user' ? null : (
+              <Space>
+                <Button
+                  color="default"
+                  variant="text"
+                  size="small"
+                  icon={<SyncOutlined />}
+                />
+                <Button
+                  color="default"
+                  variant="text"
+                  size="small"
+                  icon={<CopyOutlined />}
+                />
+              </Space>
+            )
+          }
+        />
+      ))
+    : null
+
   return (
     <div className="flex flex-4 flex-col">
+      {/* Header */}
       <div className="relative flex-none basis-18 flex items-center justify-center shadow-sm">
         <Card
           className="relative z-1 flex-1 mx-10 my-1 max-w-210 h-14"
@@ -111,16 +156,52 @@ function Chat() {
           className="absolute top-0 left-0 z-0 w-full h-full"
           src="src/assets/images/chat-header-bg.jpg"></img>
       </div>
-      <div className="flex-1">content</div>
+      {/* Record */}
+      <div className="flex-1">
+        <div className="mx-auto my-4 w-210 overflow-y-auto">
+          <Flex
+            gap="middle"
+            vertical>
+            {chatBubbleList}
+          </Flex>
+        </div>
+      </div>
+      {/* Prompt */}
+      <div className="flex-none basis-13 flex items-center justify-center mb-10">
+        <div className="w-210">
+          <Sender
+            prefix={
+              <Attachments
+                beforeUpload={() => false}
+                onChange={({ file }) => {}}
+                placeholder={{
+                  icon: <CloudUploadOutlined />,
+                  title: 'Drag & Drop files here',
+                  description: 'Support file type: image, video, audio, document, etc.',
+                }}>
+                <Button
+                  type="text"
+                  icon={<LinkOutlined />}
+                />
+              </Attachments>
+            }
+          />
+        </div>
+      </div>
     </div>
   )
 }
 
 export default function Page({}) {
+  const [activeChat, setActiveChat] = useState(null)
+
   return (
     <div className="flex w-full h-full">
-      <ChatHistory />
-      <Chat />
+      <HistoryPanel
+        activeChat={activeChat}
+        setActiveChat={setActiveChat}
+      />
+      <ChatPanel activeChat={activeChat} />
     </div>
   )
 }
